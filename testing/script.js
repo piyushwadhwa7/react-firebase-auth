1ad7d689e33c3938a416643a064a92f4f2d4b50c
Piyush Wadhwa <piyush.wadhwa.703@gmail.com>
2022-03-06 11:08:47 +0530

    changed checks

--- testing/script.js
+++ testing/script.js
@@ -6,13 +6,13 @@ import { check } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '2m', target: 10 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
-    { duration: '5m', target: 20 }, // stay at 60 users for 10 minutes
-    { duration: '3m', target: 40 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
-    { duration: '2m', target: 40 }, // stay at 100 users for short amount of time (peak hour)
-    { duration: '3m', target: 50 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
-    { duration: '10m', target: 50 }, // continue at 60 for additional 10 minutes
-    { duration: '5m', target: 0 }, // ramp-down to 0 users
+    { duration: '2m', target: 50 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
+    //{ duration: '5m', target: 20 }, // stay at 60 users for 10 minutes
+    //{ duration: '3m', target: 40 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
+    //{ duration: '2m', target: 40 }, // stay at 100 users for short amount of time (peak hour)
+    //{ duration: '3m', target: 50 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
+    //{ duration: '10m', target: 50 }, // continue at 60 for additional 10 minutes
+    //{ duration: '5m', target: 0 }, // ramp-down to 0 users
   ],
   thresholds: {
     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
